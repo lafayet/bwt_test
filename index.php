ec58694bc72008e7753a01c9c5dafffa67b4dcca
lafayet <34798302+lafayet@users.noreply.github.com>
2018-09-24 22:01:59 +0300

    Setting up project
    Bootstrap lib added

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bwt Test</title>
+
+    <!-- Bootstrap -->
+    <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="libs/bootstrap/js/bootstrap.min.js"></script>
+	
+	
+  </head>
+  <body>
+	<div class="container-fluid">
+		<h1>Welcome at our BWT test-site</h1>
+	</div>
+	<div class="container">
+		<h2>Please register or login to view contents</h2>
+</div>
+  </body>
+</html>
