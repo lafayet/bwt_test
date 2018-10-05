c76eea0beb66217af6615ded44c4f44640033dcd
lafayet <34798302+lafayet@users.noreply.github.com>
2018-10-05 12:15:22 +0300

    View for feedback done

--- application/views/view_feedback.php
+++ application/views/view_feedback.php
@@ -1,8 +1,33 @@
 <div class = "container border-container">
 	<div class = "row login-page-header" align = "center">
 		<div class="col-md-12">
-			<h1>ПОГОДА В ЗАПОРОЖЬЕ</h1><br><br>
-				In development
+			<h1>ПОГОДА В ЗАПОРОЖЬЕ</h1>
 		</div>
 	</div>
+	<div class = "row feedback-row">
+		<div class="col-md-12">
+			<form action="" method="post">
+				<br>
+				Ваше имя:* <br>
+				<input type="text" name="name"><br>
+				<br>
+				E-Mail:* <br>
+				<input type="email" name="email"> <br>
+				<br>
+				Отзыв:* <br>
+				<textarea name="feedback" cols="80" rows="6"> </textarea><br>
+				<br>
+				<input type="submit" value="Отправить отзыв" name="btn"
+				style="width: 150px; height: 30px;">
+			</form>
+		</div>
+	</div>
+	<div class = "row back">
+		<div class="col-md-12">
+			<br>
+			<a href="http:\\localhost\bwt_test\main">Назад</a>
+			<br>
+			<br>
+		</div>	
+	</div>	
 </div>
